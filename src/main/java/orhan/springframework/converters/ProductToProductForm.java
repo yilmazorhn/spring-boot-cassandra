724171ea433dbcb88fff17cec33dc57408f105a6
Orhan <orhan.yilmaz@kafein.local>
2023-09-11 18:09:13 +0300

    Initial commit

---
+++ src/main/java/orhan/springframework/converters/ProductToProductForm.java
@@ -0,0 +1,22 @@
+package orhan.springframework.converters;
+
+import orhan.springframework.commands.ProductForm;
+import orhan.springframework.domain.Product;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+@Component
+public class ProductToProductForm implements Converter<Product, ProductForm> {
+    @Override
+    public ProductForm convert(Product product) {
+        ProductForm productForm = new ProductForm();
+        productForm.setId(product.getId());
+        productForm.setDescription(product.getDescription());
+        productForm.setPrice(product.getPrice());
+        productForm.setImageUrl(product.getImageUrl());
+        return productForm;
+    }
+}
---
+++ src/main/java/orhan/springframework/services/ProductServiceImpl.java
@@ -0,0 +1,61 @@
+package orhan.springframework.services;
+
+import orhan.springframework.commands.ProductForm;
+import orhan.springframework.converters.ProductFormToProduct;
+import orhan.springframework.domain.Product;
+import orhan.springframework.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+    private ProductFormToProduct productFormToProduct;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository, ProductFormToProduct productFormToProduct) {
+        this.productRepository = productRepository;
+        this.productFormToProduct = productFormToProduct;
+    }
+
+
+    @Override
+    public List<Product> listAll() {
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add); //fun with Java 8
+        return products;
+    }
+
+    @Override
+    public Product getById(UUID id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Product saveOrUpdate(Product product) {
+        productRepository.save(product);
+        return product;
+    }
+
+    @Override
+    public void delete(UUID id) {
+        productRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Product saveOrUpdateProductForm(ProductForm productForm) {
+        Product savedProduct = saveOrUpdate(productFormToProduct.convert(productForm));
+
+        System.out.println("Saved Product Id: " + savedProduct.getId());
+        return savedProduct;
+    }
+}
